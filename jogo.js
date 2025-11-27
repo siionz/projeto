53ef17ab310f516d4b1e31130533269727a99755
siionz <gabsoouza1@gmail.com>
2025-11-27 18:39:36 -0300

    O

--- jogo.js
+++ jogo.js
@@ -10,7 +10,7 @@ const jump = () => {
         mario.classList.add('jump');
         setTimeout(() => {
             mario.classList.remove('jump');
-        }, 700); 
+        }, 700);
     }
 };
 
@@ -20,33 +20,55 @@ document.getElementById("jumpBtn").addEventListener("click", jump);
 
 function startScore() {
     score = 0;
-
     scoreInterval = setInterval(() => {
         score++;
         scoreElement.textContent = "high score: " + score;
-    }, 1);
+    }, 10);
 }
 
 startScore();
 
-const loop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    if (pipePosition <= 95 && pipePosition > 0 && marioPosition < 50) {
+let pipeX = window.innerWidth;  
+let speed = 4;                  
+let lastTime = 0;
+let gameOver = false;
 
-        clearInterval(scoreInterval);
+function movePipe(timestamp) {
 
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
+    if (lastTime === 0) lastTime = timestamp;
+    const delta = timestamp - lastTime;
+    lastTime = timestamp;
 
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
+    if (!gameOver) {
 
-        mario.src = './media/game-over.png';
-        mario.style.width = '45px';
-        mario.style.marginLeft = '60px';
+        pipeX -= speed * (delta / 16); 
 
-        clearInterval(loop);
+        if (pipeX < -60) {
+            pipeX = window.innerWidth;
+        }
+
+        pipe.style.left = pipeX + "px";
+
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+
+        if (pipeX <= 95 && pipeX > 0 && marioPosition < 50) {
+            gameOver = true;
+
+            clearInterval(scoreInterval);
+
+            mario.style.animation = 'none';
+            mario.src = './media/game-over.png';
+            mario.style.width = '45px';
+            mario.style.marginLeft = '60px';
+        }
     }
-}, 10);
+
+    requestAnimationFrame(movePipe);
+}
+
+requestAnimationFrame(movePipe);
+
+setInterval(() => {
+    speed += 0.8;
+}, 30000);
